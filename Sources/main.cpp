b679f6ef09b9a49203bdf35aa9d1ec129d938ec1
grayciex <49578879+grayciex@users.noreply.github.com>
2025-10-30 21:14:43 +0900

    fix

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -5,7 +5,7 @@
 
 namespace CTRPluginFramework
 {
-    const PluginMenu *menu = nullptr;
+    PluginMenu *menu = nullptr;
     const int version_major = 0;
     const int version_minor = 0;
     const int version_revision = 0;
@@ -71,7 +71,7 @@ exit:
     void CommonMenu(PluginMenu &menu) {
         menu += new MenuFolder("Analyze", "", {
             new MenuEntry("Memory Viewer", MemoryViewer, MemoryViewer_Menu),
-            new MenuEntry("Replace", nullptr, Replace4Byte),
+            new MenuEntry("Replace", nullptr, Replace),
             new MenuEntry("HudMenu", HudMenu),
         });
     }
